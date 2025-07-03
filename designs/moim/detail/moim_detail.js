e9eaa4f9b3c50c4387cd057b1888392ed98ae296
jiwon lee <leejw1757@naver.com>
2025-07-03 23:08:02 +0900

    HTML : 모임 상세 수정

--- designs/moim/detail/moim_detail.js
+++ designs/moim/detail/moim_detail.js
@@ -1,7 +1,7 @@
-{/* 
+
     //모임상세설명 부분 클릭 시 오버플로우 -> display 구현
-    const moreButton = document.getElementById('more-button');
-    const content = document.getElementById('content'); */}
+    const moreButton = document.getElementById('moreInfo');
+    const content = document.getElementById('content');
 
     moreButton.addEventListener('click', function() {
     if (content.style.overflow === 'hidden') {
@@ -13,5 +13,44 @@
     }
     });
 
+// 모달창 열기+닫기(모달 제외 배경 클릭 시 close)
+function openModal(button) {
+  const content = button.previousElementSibling.textContent;
+  document.getElementById('reviewText').textContent = content;
+  document.querySelector('.modal').style.display = 'block';
+  document.querySelector('.modal-backdrop').style.display = 'block';
+}
+
+function closeModal() {
+  document.querySelector('.modal').style.display = 'none';
+  document.querySelector('.modal-backdrop').style.display = 'none';
+}
+
+function backDropClick(event){
+    const modalContent = document.querySelector('.modal-content');
+    if(!modalContent.contains(e.target)){
+        closeModal();
+    }
+}
 
     
+
+//리뷰칸에 들어가는 글자 수 자르기
+const truncateReviewText = () => {
+  const spans = document.querySelectorAll(".review span");
+
+  spans.forEach(span => {
+    const fullText = span.textContent.replace("더보기", "").trim();
+    const maxLength = 28;
+
+    if (fullText.length > maxLength) {
+      span.childNodes[0].textContent = fullText.slice(0, maxLength - 3) + "... ";
+    }
+  });
+};
+
+
+window.onload = () => {
+    truncateReviewText();
+}
+
