24a55adcd22efd08c149c9321c6c1b872fdf8fda
jiwon lee <leejw1757@naver.com>
2025-07-04 23:50:11 +0900

    HTML : 모임 상세, 모임후기 등록 피드백 적용

--- designs/moim/detail/moim_detail.js
+++ designs/moim/detail/moim_detail.js
@@ -1,22 +1,44 @@
 
     //모임상세설명 부분 클릭 시 오버플로우 -> display 구현
-    const moreButton = document.getElementById('moreInfo');
+    const moreInfo = document.getElementById('moreInfo');
     const content = document.getElementById('content');
+    const fullText = content.innerHTML.trim();
+    const maxLength = 1000;
 
-    moreButton.addEventListener('click', function() {
-    if (content.style.overflow === 'hidden') {
-        content.style.overflow = 'visible';
-        moreButton.textContent = '간략히'; // 또는 다른 텍스트로 변경
+    if (fullText.length > maxLength) {
+    const shortText = fullText.slice(0, maxLength) + '...';
+    content.innerHTML = shortText;
+
+    moreInfo.style.display = "block"; 
     } else {
-        content.style.overflow = 'hidden';
-        moreButton.textContent = '더보기'; // 또는 다른 텍스트로 변경
+    moreInfo.style.display = "none"; 
     }
-    });
 
-// 모달창 열기+닫기(모달 제외 배경 클릭 시 close)
+    let isExpanded = false;
+
+    moreInfo.addEventListener('click', () => {
+    if (!isExpanded) {
+        content.innerHTML = fullText;
+        moreInfo.textContent = "접기";
+        isExpanded = true;
+    } else {
+        content.innerHTML = fullText.slice(0, maxLength)+'...';
+        moreInfo.textContent = "더보기";
+        isExpanded = false;
+    }
+});
+
+// 리뷰 클릭 : 모달창 열기+닫기 (모달 제외 배경 클릭 시 close)
 function openModal(button) {
-  const content = button.previousElementSibling.textContent;
+  // 리뷰 내용 가져오기
+  const span = button.closest('.review').querySelector('span');
+  const content = span.getAttribute('data-fulltext');
+
+
+  // 모달 텍스트 넣기(모달의 p태그 id가 reviewText)
   document.getElementById('reviewText').textContent = content;
+
+  // 모달 표시
   document.querySelector('.modal').style.display = 'block';
   document.querySelector('.modal-backdrop').style.display = 'block';
 }
@@ -43,6 +65,7 @@ const truncateReviewText = () => {
     const fullText = span.textContent.replace("더보기", "").trim();
     const maxLength = 28;
 
+    span.setAttribute("data-fulltext", fullText);
     if (fullText.length > maxLength) {
       span.childNodes[0].textContent = fullText.slice(0, maxLength - 3) + "... ";
     }
